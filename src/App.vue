5eda3b442c489c372e1b7e2dc827f69bcdfc7930
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-21 13:43:59 +0800

    others:implement simple calculater

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <p>{{count}}</p>
+    <input type="button" value="点我+" @click="add">
+    <input type="button" value="点我-" @click="sub">
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      count:0
+    };
+  },
+  methods:{
+    add(){
+      this.count++;
+    },
+    sub(){
+      this.count--;
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
